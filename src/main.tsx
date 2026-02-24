efc0068c0cd822222d61fed246d446f0c59f8280
fglez <fernando@idos.network>
2026-02-24 19:50:19 +0200

    feat: add default pending component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Spinner } from "@/components/ui/spinner";
 import { AnchoredToastProvider, ToastProvider } from "@/components/ui/toast";
 import { AppKitProvider } from "./lib/appkit";
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultPendingComponent: () => (
+    <div className="grid h-full place-content-center">
+      <Spinner className="size-6" />
+    </div>
+  ),
+});
 
 declare module "@tanstack/react-router" {
   // biome-ignore lint/style/useConsistentTypeDefinitions: module augmentation requires interface
